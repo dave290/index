328f72dd0ee144aced70bca4931950847ab4f147
David <physicsmandave@gmail.com>
2022-06-19 21:23:56 -0500

    added gridlines to meta-index

--- meta_index.py
+++ meta_index.py
@@ -15,6 +15,8 @@ ax.set_xlabel("GLONG (degrees)")
 ax.set_ylabel("GLAT (degrees)")
 ax.set_xlim(0,360)
 ax.set_ylim(-90,90)
+ax.set_xticks([0,20,40,60,80,100,120,140,160,180,200,220,240,260,280,300,320,340,360])
+ax.set_yticks([-90,-80,-70,-60,-50,-40,-30,-20,-10,0,10,20,30,40,50,60,70,80,90])
 
 #read filenames and sort by prefix
 os.chdir('index_archive')
@@ -50,6 +52,6 @@ for i in range(360):
     x.append(i)
     y.append(0)
 ax.scatter(x,y,marker=".")
-
+plt.grid()
 plt.show()
 exit()
